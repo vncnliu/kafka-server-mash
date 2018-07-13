7620e1a603c54781007e232d2bc09ee986ab69a2
vncnliu <vncnliu@gmail.com>
2018-07-13 19:17:12 +0800

    kafka处理

--- kafka-server-mash-store/src/main/java/top/vncnliu/carve/server/mash/kafka/store/ServerMashStore.java
+++ kafka-server-mash-store/src/main/java/top/vncnliu/carve/server/mash/kafka/store/ServerMashStore.java
@@ -3,18 +3,24 @@ package top.vncnliu.carve.server.mash.kafka.store;
 import com.alibaba.fastjson.JSONObject;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.KafkaMessageListenerContainer;
+import org.springframework.kafka.listener.MessageListener;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -29,7 +35,17 @@ import java.util.concurrent.ConcurrentHashMap;
 public class ServerMashStore {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServerMashStore.class);
+        //SpringApplication.run(ServerMashStore.class);
+        ContainerProperties containerProps = new ContainerProperties("topic1");
+        containerProps.setGroupId("test");
+        containerProps.setMessageListener((MessageListener<String, String>) data -> {
+            System.out.println(data.value());
+        });
+        DefaultKafkaConsumerFactory<String, String> cf =
+                new DefaultKafkaConsumerFactory<>(new HashMap<>());
+        KafkaMessageListenerContainer<String, String> container =
+                new KafkaMessageListenerContainer<>(cf, containerProps);
+        container.start();
     }
 
     public static ConcurrentHashMap<String,DeferredResult> requestHold = new ConcurrentHashMap<>();
